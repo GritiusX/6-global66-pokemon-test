63ad3a1282284395d9c92dedcd95959f028cb3ed
Guido Pawluk <guidopawluk@gmail.com>
2023-11-28 16:25:18 -0300

    Deleted one dependency but didnt take it out of main.js - all working now

--- src/main.js
+++ src/main.js
@@ -2,12 +2,7 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router/router";
-import Vue3Toastify from "vue3-toastify";
 import { createPinia } from "pinia";
 const pinia = createPinia();
 
-createApp(App)
-	.use(router)
-	.use(pinia)
-	.use(Vue3Toastify, { autoClose: 3000 })
-	.mount("#app");
+createApp(App).use(router).use(pinia).mount("#app");
